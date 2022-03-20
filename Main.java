6fbc78285ca65ed03872b703f7538b2b2c94a004
Manikanta <manikantajoy.tangudu@gmail.com>
2022-03-21 02:00:24 +0530

    lockedme

---
+++ Main.java
@@ -0,0 +1,15 @@
+package gousia;
+public class Main {
+
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\manik\\OneDrive\\Desktop\\gousiaasses\\assesment"; 
+
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+	
+
+}
